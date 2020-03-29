b22894f99015002c1c801f3b11e3ec5c54c1d309
unknown <philipstan5130@gmail.com>
2020-03-29 19:15:59 +0700

    Carousel updated to current version

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-// $(".nav-link").hover(
-//     function(){
-//     $(".active").removeClass("active");
-//     $(this).addClass("active");
-// },
-// function(){
-//     $(this).removeClass("active");
-//     $(".on").addClass("active")
-// });
-
 $(".nav-link").click(function(){
     $(".active").removeClass("active");
     $(".on").removeClass("on");
